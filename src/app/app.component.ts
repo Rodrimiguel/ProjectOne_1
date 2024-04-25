4fe8f61d1c75110431bb8f33e1621fbeef91dc46
Rodrigo M Foggia <rodrigomfoggia84@gmail.com>
2024-04-25 18:05:20 -0300

    Envío de datos entre Componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-//import { HijoComponent } from './componentes/hijo/hijo.component';
+import { HijoComponent } from './componentes/hijo/hijo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule,],
+  imports: [RouterOutlet,CommonModule,HijoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/componentes/hijo/hijo.component.ts
+++ src/app/componentes/hijo/hijo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hijo',
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './hijo.component.css'
 })
 export class HijoComponent {
+  //@input me permite recibir el atributo desde el componente padre
+  @Input("propiedadhijo") datohijo: any;
+
+  constructor() {
+    
+  }
+
 
 }
