0ec1be820cad306fd07ba47bb54b707baeb65f41
Rodrigo M Foggia <rodrigomfoggia84@gmail.com>
2024-04-25 17:50:28 -0300

    Add Class Component to DOM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+//import { HijoComponent } from './componentes/hijo/hijo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ProjectOne';
+  title = 'clasedos';
+
+  nombre: String = "Javier";
+  numero = 152;
+  numero1 = 1123123;
+  resultado = 3333;
+  //atajo para identar el codigo : alt + shift + "f"
+  datos: any = {
+    nombre: "Javier",
+    apellido: "Bardem",
+    edad: 28,
+    documento: "25123654"
+  }
+
 }
